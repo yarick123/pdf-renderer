cef137196c889b59427f3c1bf94ff88284cb63fe
yarick123 <yarick123@gmail.com>
2012-10-16 18:19:11 +0200

    use com.sun.pdfview.font.cid.PDFCMap instead of com.sun.pdfview.font.PDFCMap (it seems, that the class should be moved, but was just copied without changing of imports and deleting the old class)

--- src/com/sun/pdfview/font/PDFFontEncoding.java
+++ src/com/sun/pdfview/font/PDFFontEncoding.java
@@ -8,12 +8,12 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
@@ -27,6 +27,7 @@ import java.util.List;
 import java.util.Map;
 
 import com.sun.pdfview.PDFObject;
+import com.sun.pdfview.font.cid.PDFCMap;
 
 /**
  * The PDFFont encoding encapsulates the mapping from character codes
@@ -46,7 +47,7 @@ public class PDFFontEncoding {
     /** Encoding types */
     private static final int TYPE_ENCODING = 0;
     private static final int TYPE_CMAP = 1;
-    /** 
+    /**
      * the base encoding (an array of integers which can be mapped to names
      * using the methods on FontSupport
      */
